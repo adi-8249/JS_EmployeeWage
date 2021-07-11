6c887b0a2525e8474c8c8317bad379b4fd3b74a2
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-11 20:38:48 +0530

    [Anjaneya]added:Checking wheather employee is present or not

--- WelcomeMessage.js
+++ WelcomeMessage.js
@@ -1,1 +1,13 @@
 console.log("Welcome to javascript employee wage program");
+//UC1_EmployeeAttendance check
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+console.log(empCheck);
+if (empCheck == IS_ABSENT) 
+{
+  console.log("Employee is absent");
+} 
+else 
+{
+  console.log("Employee is present");
+}
